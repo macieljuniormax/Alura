443f9ce5429a9be6171e8a615c7de261ebeb7244
macieljuniormax <maciel_custodio@icloud.com>
2025-07-03 18:04:57 -0300

    Add initial implementation of AppDelegate and SceneDelegate, remove storyboard and ViewController
    - Created AppDelegate.swift and SceneDelegate.swift to manage application lifecycle.
    - Removed Main.storyboard and ViewController.swift to streamline the project structure.
    - Added HomeTableViewHeader.xib and its corresponding Swift file for custom header view.
    - Updated ViagemTableViewCell.xib to enhance cell layout and design.
    - Implemented Auto Layout constraints in HomeTableViewHeader.xib for better responsiveness.

--- Swift/02 - iOS: Auto Layout Com Constraints/Alura Viagens/Alura Viagens/View/Header/HomeTableViewHeader.swift
+++ Swift/02 - iOS: Auto Layout Com Constraints/Alura Viagens/Alura Viagens/View/Header/HomeTableViewHeader.swift
@@ -13,8 +13,14 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var bannerView: UIView!
     
     internal func configuraView() {
         self.headerView.backgroundColor = UIColor(red: 30.0/255, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1.0)
+        self.headerView.layer.cornerRadius = 500
+        self.headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
+        self.bannerView.layer.cornerRadius = 10
+        self.bannerView.layer.masksToBounds = true
     }
 }
