bf26599c08da5519093f4627ed3ea24b95b28cf6
Maciel Custódio <maciel_custodio@icloud.com>
2026-04-08 22:44:10 -0300

    ✨ feat: add ProductDetailHeaderView and ProductDetailQuantityView; refactor StoreDetailView to integrate new components for improved product display

--- Swift/03 - iOS: Construindo Componentes e Layouts/Chef Delivery/Chef Delivery/ProductView/ProductDetailView.swift
+++ Swift/03 - iOS: Construindo Componentes e Layouts/Chef Delivery/Chef Delivery/ProductView/ProductDetailView.swift
@@ -8,72 +8,27 @@
 import SwiftUI
 
 struct ProductDetailView: View {
-    @State private var productQuantity: Int = 1
-    
     internal let product: ProductType
     
     var body: some View {
-        VStack(alignment: HorizontalAlignment.leading, spacing: 16) {
-            Image(product.image)
-                .resizable()
-                .scaledToFill()
-                .frame(height: 300)
-                .frame(maxWidth: CGFloat.infinity)
-                .clipped()
-            
-            VStack(alignment: HorizontalAlignment.leading, spacing: 16) {
-                Text(product.name)
-                    .font(Font.title)
-                    .fontWeight(Font.Weight.bold)
-                
-                Text(product.description)
-                    .multilineTextAlignment(TextAlignment.leading)
-                
-                Text(product.formattedPrice)
-                    .font(Font.title3)
-                    .fontWeight(Font.Weight.bold)
-            }
-            .padding(Edge.Set.horizontal, 16)
-            .foregroundColor(Color.black)
-            
-        }
+        ProductDetailHeaderView(product: product)
         
         Spacer()
         
-        VStack(spacing: 16) {
-            Text("Quantidade")
-                .font(Font.title3)
-                .fontWeight(Font.Weight.bold)
-                .foregroundColor(Color.black)
-            
-            HStack(spacing: 16) {
-                Button {
-                    if productQuantity > 1 { productQuantity -= 1 }
-                    
-                } label: {
-                    Image(systemName: "minus.circle.fill")
-                        .font(Font.title)
-                        .fontWeight(Font.Weight.bold)
-                }
-                
-                Text("\(productQuantity)")
-                    .font(Font.title)
-                    .fontWeight(Font.Weight.bold)
-                    .foregroundColor(Color.black)
-                
-                Button {
-                    productQuantity += 1
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                        .font(Font.title)
-                        .fontWeight(Font.Weight.bold)
-                }
-            }
-        }
+        ProductDetailQuantityView()
         
         Spacer()
+        
+        ProductDetailButtonView()
+    }
+}
 
-        
+#Preview(traits: .sizeThatFitsLayout) {
+    ProductDetailView(product: storesMock[0].products[0])
+}
+
+struct ProductDetailButtonView: View {
+    var body: some View {
         Button {
             print("O botão foi pressionado 🔘")
         } label: {
@@ -87,8 +42,4 @@ struct ProductDetailView: View {
         .foregroundColor(Color.red500)
         .fontWeight(Font.Weight.bold)
     }
-}
-
-#Preview(traits: .sizeThatFitsLayout) {
-    ProductDetailView(product: storesMock[0].products[0])
 }
--- Swift/03 - iOS: Construindo Componentes e Layouts/Chef Delivery/Chef Delivery/StoresView/StoreDetailView/StoreDetailView.swift
+++ Swift/03 - iOS: Construindo Componentes e Layouts/Chef Delivery/Chef Delivery/StoresView/StoreDetailView/StoreDetailView.swift
@@ -10,98 +10,17 @@ import SwiftUI
 struct StoreDetailView: View {
     @Environment(\.presentationMode) var presentationMode
     @State private var isFavorite: Bool = false
-    @State private var selectedProduct: ProductType?
- 
+    
     internal let store: StoreType
     
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: HorizontalAlignment.leading) {
-                GeometryReader { geo in
-                    let offset = geo.frame(in: .global).minY
-                    
-                    Image(store.headerImage)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(
-                            width: geo.size.width,
-                            height: offset > 0 ? 150 + offset : 150
-                        )
-                        .clipped()
-                        .offset(y: offset > 0 ? -offset : 0)
-                        .ignoresSafeArea(edges: Edge.Set.top)
-                }
-                .frame(height: 150)
-                
                 VStack(alignment: HorizontalAlignment.leading, spacing: 16) {
-                    HStack(alignment: VerticalAlignment.center) {
-                        Text(store.name)
-                            .font(Font.title)
-                            .fontWeight(Font.Weight.bold)
-                        
-                        Spacer()
-                        
-                        Image(store.logoImage)
-                    }
-                    .padding(Edge.Set.horizontal, 16)
-                    
-                    HStack(alignment: VerticalAlignment.center) {
-                        Text(store.location)
-                        
-                        Spacer()
-                        
-                        ForEach(1...store.stars, id: \.self) { _ in
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color.yellow)
-                        }
-                    }
-                    .padding(Edge.Set.horizontal, 16)
-                    
-                    Text("Produtos")
-                        .font(Font.title2)
-                        .fontWeight(Font.Weight.bold)
-                        .padding(Edge.Set.horizontal, 16)
-                    
-                    ForEach(store.products) { product in
-                        Button {
-                            selectedProduct = product
-                        } label: {
-                            HStack(alignment: VerticalAlignment.top, spacing: 8) {
-                                VStack(alignment: HorizontalAlignment.leading) {
-                                    Text(product.name)
-                                        .fontWeight(Font.Weight.bold)
-                                        .foregroundColor(Color.black)
-                                    
-                                    Text(product.description)
-                                        .foregroundColor(Color.black.opacity(0.7))
-                                        .multilineTextAlignment(TextAlignment.leading)
-                                    
-                                    Spacer()
-                                    
-                                    Text(product.formattedPrice)
-                                        .foregroundStyle(Color.black)
-                                }
-                                
-                                Spacer()
-                                
-                                Image(product.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .cornerRadius(15)
-                                    .frame(height: 100)
-                                    .shadow(color: Color.black.opacity(0.3),
-                                            radius: 20, x: 6, y: 8)
-                            }
-                            .sheet(item: $selectedProduct) { product in
-                                ProductDetailView(product: product)
-                            }
-                        }
-                    }
-                    .padding(Edge.Set.horizontal, 16)
+                    StoreDetailHeaderView(store: store)
+                    StoreDetailProductsView(products: store.products)
                 }
-                
-                
-             }
+            }
             .navigationTitle(store.name)
             .navigationBarTitleDisplayMode(NavigationBarItem.TitleDisplayMode.inline)
         }
